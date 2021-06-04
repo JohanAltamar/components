1a0ee42f62530e25909c1fbbf1bbe26ba1c772b0
Johan Altamar <johan.altamar.dev@gmail.com>
2021-06-04 11:23:28 -0500

    fix className override

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   variation?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'link';
+  className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ variation = "light", ...rest}) => {
-  let className: string  = rest.className || '';
-  className = `${className} btn btn-${variation}`; 
+const Button: React.FC<ButtonProps> = ({ variation = "light", ...props}) => {
+  let {className, ...rest} = props;
+  
+  className = className
+    ? `${className} btn btn-${variation}`
+    : `btn btn-${variation}`; 
 
   return (
     <button className={className} {...rest} />
